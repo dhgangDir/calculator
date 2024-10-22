cf3d40991f32f7765b9e097c767877bd566b68f4
developer2 <dev2@gamil.com>
2024-10-23 01:03:40 +0900

    Modify ctrl.py to connect btn1 to calculate method

--- ctrl.py
+++ ctrl.py
@@ -1,9 +1,12 @@
-# ch 5.2.1 ctrl.py
+# ch 6.2.2 ctrl.py
 class Control:
 
     def __init__(self, view):
         self.view = view
         self.connectSignals()
+
+    def calculate(self): # calculate 메서드 추가. 내용은 추후에 작성
+        pass
 
     def connectSignals(self):
         self.view.btn1.clicked.connect(self.view.activateMessage)
